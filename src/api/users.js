a5a96d5fedb219bc8e0605e2088fbdb1faea1b52
jiangjians <1455928402@qq.com>
2022-06-26 18:04:20 +0800

    完成登录功能

---
+++ src/api/users.js
@@ -0,0 +1,17 @@
+import request from '@/utils/request'
+
+export const getSmsCode = (mobile) => {
+  return request({
+    url: `/sms/codes/${mobile}`
+  })
+}
+
+export const login = ({ mobile, code }) => {
+  return request({
+    method: 'POST',
+    url: '/authorizations',
+    data: {
+      mobile, code
+    }
+  })
+}
